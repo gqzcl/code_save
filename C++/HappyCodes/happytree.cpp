1cef95b0ae66ca62d4ab2902879e15c915e53c15
gqzcl <1476048558@qq.com>
2018-12-01 17:46:36 +0800

    111

--- C++/HappyCodes/happytree.cpp
+++ C++/HappyCodes/happytree.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
+#include<stack>
 using namespace std;
 const int MAX_TREE_SIZE=100;
 typedef struct BiTNode{
     char data;
     struct BiTNode *lchild,*rchild;
-}BiTnode,*BiTree;
+}BiTNode,*BiTree;
 void CreateBiTree(BiTree &T){
     char ch;
     scanf("%c",&ch);
@@ -12,24 +13,50 @@ void CreateBiTree(BiTree &T){
         T=NULL;
         return;
     }else{
-        if(!(T=(BiTnode*)malloc(sizeof(BiTNode))))
+        if(!(T=(BiTNode*)malloc(sizeof(BiTNode))))
             return;
         T->data=ch;
         CreateBiTree(T->lchild);
         CreateBiTree(T->rchild);
     }
 }
-void PreOrderTraverse(BiTree T){   
-    if(T){
-        printf("%c",T->data);
-        PreOrderTraverse(T->lchild);
-        PreOrderTraverse(T->rchild);                        
-    }       
+// void PreOrderTraverse(BiTree T){   
+//     if(T){
+//         PreOrderTraverse(T->lchild);
+//         cout<<T->data;
+//         PreOrderTraverse(T->rchild);                        
+//     }else{
+//         cout<<'#';
+//     }    
+//     return;
+// }
+void PreOrderTraverse(BiTree T){
+    stack<BiTNode> S;
+    int i=0;
+    //T=NULL;
+    S.push(T);
+    //cout<<S.top();
+    //cout<<S.empty();
+    while(!S.empty()){
+        while(S.top()){
+            S.push(S.top()->lchild);
+            //cout<<S.top();
+        }
+        //cout<<i++;
+        while(!S.top()){
+            S.pop();
+            cout<<'#';
+        }
+        cout<<S.top()->data;
+        S.top()=S.top()->rchild;
+        
+    }
     return;
 }
+//AB0CD000E0FGH00K000
+
 int main(){
     BiTree T;
-    char e;
     CreateBiTree(T);
     PreOrderTraverse(T);
     printf("\n");
